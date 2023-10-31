5d456850dc37441832603883f818411853883c09
Subham Sidhartha <subham.sidhartha@dokonaly.com>
2023-10-31 15:46:29 +0530

    logger added for upcomming request

--- app.js
+++ app.js
@@ -7,12 +7,19 @@ import fs from "fs"
 import bookRouter from "./routes/bookRoutes.js";
 import CONFIG from "./config/config.js";
 import bodyParser from "body-parser";
+import path from "path";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const loadJSON = (path) => JSON.parse(fs.readFileSync(new URL(path, import.meta.url)));
 const CONFIGJSON = loadJSON('./config/config.json');
 
+
 const app = express(); // Initializing ExpressJS
 app.use(express.json())
-
 app.use(bodyParser.json())
 
 app.use(cors({
@@ -22,7 +29,6 @@ app.use(cors({
     allowedHeaders: ["Origin", "X-Requested-with", "Content-Type", "Accept", "Authorization"],
 }))
 
-
 mongoose.connect(CONFIG.DB_URL, { useNewUrlParser: true })
     .then(() => {
         console.log("Mongodb is connected successfully.✔🟢✅");
@@ -31,12 +37,11 @@ mongoose.connect(CONFIG.DB_URL, { useNewUrlParser: true })
         console.log(err)
     })
 
-
-app.use("/user", userRouter)
-app.use("/book", bookRouter)
+const paths = path.join(__dirname, 'access.log')
+const accessLogStream = fs.createWriteStream(paths, { flags: 'a' })
 
 app.use(morgan(
-    function (tokens, req, res) {
+    (tokens, req, res) => {
 
         if (tokens.method(req, res) == 'POST') {
             return [
@@ -59,8 +64,12 @@ app.use(morgan(
                 new Date().toJSON()
             ].join(' ')
         }
-    }
-))
+
+    }, { stream: accessLogStream }));
+
+app.use("/user", userRouter)
+app.use("/book", bookRouter)
+
 
 app.listen(process.env.PORT || 3000, () => {
     return console.log(`Express is running on port 🤣 ${process.env.PORT || 3000}`)
--- config/config.js
+++ config/config.js
@@ -1,7 +1,6 @@
 import fs from "fs"
 import path from "path"
 import dotenv from "dotenv"
-import { URL } from 'url';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path'; 
 
